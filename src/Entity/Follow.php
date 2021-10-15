3543f86ee3556d05a9ff204e71b5ceba6dbadbf1
Julien REIGNIER <julienreignierr@gmail.com>
2021-10-15 19:10:29 +0200

    wip

--- src/Entity/Follow.php
+++ src/Entity/Follow.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\FollowRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=FollowRepository::class)
