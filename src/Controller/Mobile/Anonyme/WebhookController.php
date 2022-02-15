30e7bd1315bf8dfe0ea874448e757182becc20d5
Julien REIGNIER <julienreignierr@gmail.com>
2022-02-15 17:40:02 +0100

    wip

--- src/Controller/Mobile/Anonyme/WebhookController.php
+++ src/Controller/Mobile/Anonyme/WebhookController.php
@@ -142,7 +142,7 @@ class WebhookController extends Controller {
 
     if ($result["type"]) {
       // $this->get('bugsnag')->notifyException(new Exception($result["data"]));
-      $this->get('bugsnag')->notifyException(new Exception($result["data"]["object"]));
+      // $this->get('bugsnag')->notifyException(new Exception($result["data"]["object"]));
       $this->get('bugsnag')->notifyException(new Exception($result["data"]["object"]["status"]));
       $order = $orderRepo->findOneByPaymentId($result["data"]->object->id);
 
