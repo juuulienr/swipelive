43011921b486d4cfa0dcfa5d2dfdeaf8221f26fd
Julien REIGNIER <julienreignierr@gmail.com>
2023-04-02 17:13:55 +0400

    wip

--- src/Controller/App/ShippingAPIController.php
+++ src/Controller/App/ShippingAPIController.php
@@ -283,34 +283,38 @@ class ShippingAPIController extends Controller {
       // créer l'étiquette 
       $data = [
         "order_id" => "Test_order_id2", 
-        "process_shipment" => true, 
         "shipment" => [
           "id" => 0, 
           "type" => 2, 
-          "shipment_date" => "2023-04-24", 
-          "service_code" => "DOM", 
+          "dropoff": false, // true if collection point
           "service_id" => "58da4d87-f7e1-4bb7-901e-ff6b52062b4c", 
-          "delivery_type" => "DELIVERY_TO_COLLECTION_POINT", 
+          "delivery_type" => "DELIVERY_TO_COLLECTION_POINT",  //HOME_DELIVERY
           "label_format" => "PDF", 
-          "insurance" => true, 
-          "insurance_type" => "UPELA", 
-          "insurance_price" => 100 
+          // "insurance" => true, 
+          // "insurance_type" => "UPELA", 
+          // "insurance_price" => 100 
         ], 
         "ship_from" => [
-          "address1" => "87 chemin de la lune", 
-          "lastname" => "Loic Rombai", 
-          "email" => "neoglucogenese@gmail.com", 
-          "phone" => "+33666666666", 
-          "company" => "Upela" 
+          "address1" => $vendor->getAddress(), 
+          "lastname" => $vendor->getUser()->getFullName(), 
+          "email" => $vendor->getUser()->getEmail(), 
+          "phone" => $vendor->getUser()->getPhone(), 
+          "company" => $vendor->getCompany()
         ], 
         "ship_to" => [
           "pro" => false,
-          "address1" => "54 rue du coteau", 
-          "lastname" => "Marc Reignier", 
-          "email" => "marc.reignier@laposte.net", 
-          "phone" => "+33666666667", 
-          "company" => "Upela" 
-        ] 
+          "address1" => $shippingAddress->getAddress(), 
+          "lastname" => $order->getBuyer()->getFullName(), 
+          "email" => $order->getBuyer()->getEmail(), 
+          "phone" => $shippingAddress->getPhone(), 
+          "company" => "Swipe Live" 
+        ],
+        // "dropoff_to" => [
+        //   "country_code" => "FR",
+        //   "postcode" => "69140",
+        //   "lastname" => "ESPACE DIGITAL",
+        //   "dropoff_location_id" => "006274"
+        // ]
       ]; 
 
 
@@ -323,11 +327,21 @@ class ShippingAPIController extends Controller {
 
       $result = curl_exec($ch);
       $result = json_decode($result);
-      dump($result);
       curl_close($ch);
 
+      dump($result);
 
       if ($result->success == true) {
+       // $id = $result->parcel->id;
+       // $tracking_number = $result->parcel->tracking_number;
+       // $tracking_url = $result->parcel->tracking_url;
+
+       // $order->setTrackingUrl($tracking_url);
+       // $order->setTrackingNumber($tracking_number);
+       // $order->setParcelId($id);
+       // $order->setPdf($filename);
+       // $manager->flush();
+
         // dump($result->parent_carrier);
         // dump($result->shipment_id);
         // dump($result->carrier_id);
@@ -339,173 +353,16 @@ class ShippingAPIController extends Controller {
         // dump($result->waybills_uri);
         // dump($result->pickup_code);
         // dump($result->documents);
+
+        return $this->json($order, 200, [], [
+          'groups' => 'order:read', 
+        ]);
       }
     } catch (\Exception $e) {
       return $this->json($e->getMessage(), 404);
     }
 
-
-
-  	// try {
-    //   $variable = explode(" ", trim($vendor->getAddress()), 2);
-    //   $from_address_1 = trim($variable[1]);
-    //   $from_house_number = trim($variable[0]);
-
-  	// 	$data = [
-  	// 		"parcel" => [
-  	// 			"name" => $order->getBuyer()->getFullName(), 
-  	// 			"address" => $shippingAddress->getAddress(), 
-  	// 			"house_number" => $shippingAddress->getHouseNumber(), 
-  	// 			"city" => $shippingAddress->getCity(), 
-  	// 			"postal_code" => $shippingAddress->getZip(), 
-  	// 			"country" => $shippingAddress->getCountryCode(), 
-  	// 			"telephone" => $shippingAddress->getPhone() ? $shippingAddress->getPhone() : "", 
-  	// 			"email" => $order->getBuyer()->getEmail(), 
-  	// 			"order_number" => $order->getNumber(), 
-  	// 			"weight" => $order->getWeight(),
-  	// 			"to_service_point" => $order->getServicePointId(),
-  	// 			"request_label" => true, 
-  	// 			"shipment" => [
-  	// 				"id" => $order->getShippingMethodId()
-  	// 			], 
-  	// 			"parcel_items" => [], 
-  	// 			"from_name" => $vendor->getUser()->getFullName(), 
-  	// 			"from_company_name" => $companyName, 
-  	// 			"from_address_1" => $from_address_1, 
-  	// 			"from_address_2" => "", 
-  	// 			"from_house_number" => $from_house_number,
-  	// 			"from_city" => $vendor->getCity(), 
-  	// 			"from_postal_code" => $vendor->getZip(), 
-  	// 			"from_country" => $vendor->getCountryCode(), 
-  	// 			"from_email" => $vendor->getUser()->getEmail(), 
-  	// 		]
-  	// 	]; 
-
-  	// 	$curl = curl_init();
-  	// 	curl_setopt_array($curl, [
-  	// 		CURLOPT_URL => "https://panel.sendcloud.sc/api/v2/parcels",
-  	// 		CURLOPT_RETURNTRANSFER => true,
-  	// 		CURLOPT_ENCODING => "",
-  	// 		CURLOPT_MAXREDIRS => 10,
-  	// 		CURLOPT_TIMEOUT => 30,
-  	// 		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  	// 		CURLOPT_CUSTOMREQUEST => "POST",
-  	// 		CURLOPT_POSTFIELDS => json_encode($data),
-  	// 		CURLOPT_HTTPHEADER => [
-  	// 			"Authorization: Basic MzgyNjY4NmYyZGJjNDE4MzgwODk4Y2MyNTRmYzBkMjg6MDk2ZTQ0Y2I5YjI2NDMxYjkwY2M1YjVkZWZjOWU5MTU=",
-  	// 			"Content-Type: application/json"
-  	// 		],
-  	// 	]);
-
-  		// $response = curl_exec($curl);
-  		// $result = json_decode($response);
-  		// curl_close($curl);
-
-  		// if ($result && array_key_exists("parcel",$result)) {
-  		// 	$id = $result->parcel->id;
-  		// 	$tracking_number = $result->parcel->tracking_number;
-      //   $tracking_url = $result->parcel->tracking_url;
-			// 	$url = "https://panel.sendcloud.sc/api/v2/labels/normal_printer/" . $id;
-			// 	$curl = curl_init();
-
-			// 	curl_setopt_array($curl, [
-			// 		CURLOPT_URL => $url,
-			// 		CURLOPT_RETURNTRANSFER => true,
-			// 		CURLOPT_ENCODING => "",
-			// 		CURLOPT_MAXREDIRS => 10,
-			// 		CURLOPT_TIMEOUT => 30,
-			// 		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			// 		CURLOPT_CUSTOMREQUEST => "GET",
-			// 		CURLOPT_HTTPHEADER => [
-			// 			"Authorization: Basic MzgyNjY4NmYyZGJjNDE4MzgwODk4Y2MyNTRmYzBkMjg6MDk2ZTQ0Y2I5YjI2NDMxYjkwY2M1YjVkZWZjOWU5MTU=",
-			// 			"Content-Type: application/json"
-			// 		],
-			// 	]);
-
-			// 	$content = curl_exec($curl);
-			// 	curl_close($curl);
-
-			// 	if ($content) {
-      //     $filename = md5(time().uniqid()); 
-      //     $fullname = $filename. ".pdf"; 
-      //     $filepath = $this->getParameter('uploads_directory') . '/' . $fullname;
-      //     file_put_contents($filepath, $content);
-
-      //     try {
-      //       $result = (new UploadApi())->upload($filepath, [
-      //         'public_id' => $filename,
-      //         'use_filename' => TRUE,
-      //       ]);
-
-      //       unlink($filepath);
-      //     } catch (\Exception $e) {
-      //       return $this->json($e->getMessage(), 404);
-      //     }
-      
-	  	// 		$order->setTrackingUrl($tracking_url);
-      //     $order->setTrackingNumber($tracking_number);
-	  	// 		$order->setParcelId($id);
-	  	// 		$order->setPdf($filename);
-	  	// 		$manager->flush();
-
-
-      //     if ($order->getTrackingNumber()) {
-      //       $url = "https://panel.sendcloud.sc/api/v2/tracking/" . $order->getTrackingNumber();
-      //       $curl = curl_init();
-
-      //       curl_setopt_array($curl, [
-      //         CURLOPT_URL => $url,
-      //         CURLOPT_RETURNTRANSFER => true,
-      //         CURLOPT_ENCODING => "",
-      //         CURLOPT_MAXREDIRS => 10,
-      //         CURLOPT_TIMEOUT => 30,
-      //         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      //         CURLOPT_CUSTOMREQUEST => "GET",
-      //         CURLOPT_HTTPHEADER => [
-      //           "Authorization: Basic MzgyNjY4NmYyZGJjNDE4MzgwODk4Y2MyNTRmYzBkMjg6MDk2ZTQ0Y2I5YjI2NDMxYjkwY2M1YjVkZWZjOWU5MTU=",
-      //           "Content-Type: application/json"
-      //         ],
-      //       ]);
-
-      //       $response = curl_exec($curl);
-      //       $result = json_decode($response);
-      //       curl_close($curl);
-
-      //       if ($result && array_key_exists("expected_delivery_date", $result)) {
-      //         $order->setExpectedDelivery(new \Datetime($result->expected_delivery_date));
-      //         $manager->flush();
-
-      //         foreach ($result->statuses as $status) {
-      //           $orderStatus = $statusRepo->findOneByStatusId($status->parcel_status_history_id);
-
-      //           if (!$orderStatus) {
-      //             $orderStatus = new OrderStatus();
-      //             $orderStatus->setUpdateAt(new \Datetime($status->carrier_update_timestamp));
-      //             $orderStatus->setMessage($status->carrier_message);
-      //             $orderStatus->setStatus($status->parent_status);
-      //             $orderStatus->setCode($status->carrier_code);
-      //             $orderStatus->setStatusId($status->parcel_status_history_id);
-      //             $orderStatus->setShipping($order);
-      //             $order->setShippingStatus($status->parent_status);
-      //             $order->setUpdatedAt(new \Datetime($status->carrier_update_timestamp));
-
-      //             $manager->persist($orderStatus);
-      //             $manager->flush();
-      //           }
-      //         }
-      //       }
-      //     }
-
-      //     return $this->json($order, 200, [], [
-      //       'groups' => 'order:read', 
-      //       'datetime_format' => 'd F Y à H:i' 
-      //     ]);
-      //   }
-  		// } else {
-      //   return $this->json($result->error->message, 404);
-      // }
-
-      return true;
+    return $this->json(false, 404);
   }
 
 
