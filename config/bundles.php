1bcd8d72407d9d74b79017339705c590300fa0d2
Julien REIGNIER <julienreignierr@gmail.com>
2024-05-08 17:28:19 +0100

    wip

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Bugsnag\BugsnagBundle\BugsnagBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
