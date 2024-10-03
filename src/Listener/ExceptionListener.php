565b4ea48a29cd006f9995df5056b0d3d2538b21
Julien REIGNIER <julienreignierr@gmail.com>
2024-10-03 11:28:25 +0200

    ajout retour des erreurs api en json

---
+++ src/Listener/ExceptionListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener
+{
+  public function onKernelException(ExceptionEvent $event)
+  {
+    $request = $event->getRequest();
+    $exception = $event->getThrowable();
+
+    if ($this->isApiRequest($request)) {
+      $response = new JsonResponse([
+        'error' => true,
+        'message' => $exception->getMessage(),
+      ]);
+
+      if ($exception instanceof HttpExceptionInterface) {
+        $response->setStatusCode($exception->getStatusCode());
+      } else {
+        $response->setStatusCode(500);
+      }
+
+      $event->setResponse($response);
+    }
+  }
+
+  private function isApiRequest($request): bool
+  {
+    return strpos($request->getPathInfo(), '/api') === 0 || strpos($request->getPathInfo(), '/user/api') === 0;
+  }
+}
